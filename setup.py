a36ea1505e750579e80287d2ddb3a71278755397
Alexei Znamensky <russoz@gmail.com>
2022-04-03 21:01:08 +1200

    Bump version: 0.18 → 0.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 requirements = [line.strip() for line in open('requirements.txt').readlines()]
 
 setup(name='andebox',
-      version='0.18',
+      version='0.19',
       scripts=['andebox'],
       packages=find_packages(),
       install_requires=requirements,
