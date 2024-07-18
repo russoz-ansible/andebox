827149c16308dc81d8345ed5914fa48112e92aac
Alexei Znamensky <103110+russoz@users.noreply.github.com>
2024-07-19 10:32:44 +1200

    Adding tests for action ansibletest (#23)
    * tests work locally
    
    * add skip for declared python versions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # (c) 2024, Alexei Znamensky <russoz@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+import os
 import shutil
 import subprocess
 import tempfile
@@ -13,7 +14,7 @@ import pytest
 def git_repo():
     clones = {}
 
-    temp_dir = Path(tempfile.mkdtemp(prefix="andebox-test-tmp."))
+    temp_dir = Path(tempfile.mkdtemp(prefix="andebox-test-tmp-repo."))
 
     def _shallow_clone_git_repo(repo_url):
         if repo_url not in clones:
@@ -22,6 +23,7 @@ def git_repo():
             dest = str(temp_dir / stem)
 
             # Perform shallow clone
+            print(f"Cloning repo {repo_url} => {dest}")
             subprocess.run(["git", "clone", "--depth", "1", repo_url, dest], check=True)
 
             # Store the path for later cleanup
@@ -34,3 +36,35 @@ def git_repo():
     yield _shallow_clone_git_repo
 
     shutil.rmtree(temp_dir)
+
+
+@pytest.fixture(scope="session")
+def install_andebox():
+    proj_dir = os.getcwd()
+
+    print(f"{proj_dir=}")
+
+    print("Uninstalling andebox")
+    subprocess.run(
+        ["pip", "uninstall", "-y", "andebox"],
+        check=True,
+        encoding="utf-8",
+        capture_output=True,
+    )
+    print("Installing andebox")
+    subprocess.run(
+        ["pip", "install", "-e", proj_dir],
+        check=True,
+        encoding="utf-8",
+        capture_output=True,
+    )
+
+    yield
+
+    print("Uninstalling andebox (cleanup)")
+    subprocess.run(
+        ["pip", "uninstall", "-y", "andebox"],
+        check=False,
+        encoding="utf-8",
+        capture_output=True,
+    )
