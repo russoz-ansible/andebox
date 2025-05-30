505f2b857c131aeb8f840d5b82e728d978183531
Alexei Znamensky <russoz@gmail.com>
2025-05-31 04:12:16 +1200

    feat: add module execution protocol

--- andebox/__main__.py
+++ andebox/__main__.py
@@ -1,18 +1,10 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2025, Alexei Znamensky
 # All rights reserved.
 #
 # This file is part of the Andebox project and is distributed under the terms
 # of the BSD 3-Clause License. See LICENSE file for details.
+from andebox.cli import run
 
-[pytest]
-addopts =
-    --cov=andebox --cov-report=term-missing
-    --cov-report=html:htmlcov
-    --junitxml=junit.xml
-
-testpaths =
-    tests
-
-[coverage:run]
-omit =
-    andebox/__main__.py
+if __name__ == "__main__":
+    run()
