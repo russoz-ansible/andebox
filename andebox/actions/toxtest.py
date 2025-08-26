d71106f139acffc1101c1523a7d9b83e0efd0291
Alexei Znamensky <russoz@gmail.com>
2025-08-27 00:04:00 +1200

    feat(action/tox-test): update default tox ini up to ansible-core 2.19

--- andebox/actions/toxtest.py
+++ andebox/actions/toxtest.py
@@ -1,38 +1,78 @@
 # -*- coding: utf-8 -*-
 # code: language=python tabSize=4
-# (C) 2021-2023 Alexei Znamensky
+# (C) 2021 Alexei Znamensky
 # Licensed under the MIT License. See LICENSES/MIT.txt for details.
-# SPDX-FileCopyrightText: 2021-2023 Alexei Znamensky
+# SPDX-FileCopyrightText: 2021 Alexei Znamensky
 # SPDX-License-Identifier: MIT
 import os
 import subprocess
-import sys
 
 from ..exceptions import AndeboxException
 from .base import AndeboxAction
 
 
 def _make_default_tox_ini():
-    return f""";
+    return """\
+;
 ; andebox tox-test's tox.ini -- this file is not overwritten by andebox
 [tox]
 isolated_build = true
-envlist = ac215, ac216, ac217, ac218, dev
+envlist = ac213, ac214, ac215, ac216, ac217, ac218, ac219, dev
 skipsdist = true
 
 [testenv]
 passenv = PWD, HOME
 skip_install = true
 allowlist_externals = andebox
-basepython = {sys.executable}
+commands = andebox test -- {posargs}
+
+[testenv:ac213]
+basepython = python3.10
 deps =
+  ansible-core~=2.13.0
   andebox>=0.36
-  ac215: ansible-core~=2.15.0
-  ac216: ansible-core~=2.16.0
-  ac217: ansible-core~=2.17.0
-  ac218: ansible-core~=2.18.0
-  dev: https://github.com/ansible/ansible/archive/devel.tar.gz
-commands = andebox test -- {{posargs}}
+
+[testenv:ac214]
+basepython = python3.11
+deps =
+  ansible-core~=2.14.0
+  andebox>=0.36
+
+[testenv:ac215]
+basepython = python3.11
+deps =
+  ansible-core~=2.15.0
+  andebox>=0.66
+
+[testenv:ac216]
+basepython = python3.12
+deps =
+  ansible-core~=2.16.0
+  andebox>=0.66
+
+[testenv:ac217]
+basepython = python3.12
+deps =
+  ansible-core~=2.17.0
+  andebox>=0.66
+
+[testenv:ac218]
+basepython = python3.13
+deps =
+  ansible-core~=2.18.0
+  andebox>=0.66
+
+[testenv:ac219]
+basepython = python3.13
+deps =
+  ansible-core~=2.19.0
+  andebox>=0.66
+
+[testenv:dev]
+basepython = python3.13
+deps =
+  https://github.com/ansible/ansible/archive/devel.tar.gz
+  andebox>=0.66
 """
 
 
